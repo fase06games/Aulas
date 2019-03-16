5c3ff93f167fd8576274bdd150d64776f5f383b8
HenriqueSGC <48387484+HenriqueSGC@users.noreply.github.com>
2019-03-16 07:42:33 -0300

    Nova Aula

---
+++ Aula-03/Assets/GameFiles/Scripts/MonsterCatch.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterCatch : MonoBehaviour
+{
+	void OnCollisionEnter(Collision other)
+	{
+		if(other.gameObject.tag == "Monster")
+		{
+			Destroy(other.gameObject);
+			Destroy(this.gameObject);
+		}
+	}
+}
---
+++ Aula-03/Assets/GameFiles/Scripts/PlayerAction.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAction : MonoBehaviour
+{
+	public GameObject bullet;
+	public float bulletSpeed = 10.0f;
+
+	public ArduinoListener arduino = null;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1") || arduino.IsShooting() )
+        {
+        	Shoot();
+        }
+    }
+
+    public void Shoot()
+    {
+    	GameObject createdBullet = Instantiate(bullet);
+        createdBullet.transform.position = transform.position;
+
+        Rigidbody rBullet = createdBullet.GetComponent<Rigidbody>();
+        Camera cam = GetComponentInChildren<Camera>();
+
+        rBullet.velocity = cam.transform.rotation * Vector3.forward * bulletSpeed;
+    }
+
+    
+}
