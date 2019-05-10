243169d62c9e7c5235b0ea8b028e75c5a6945a59
HenriqueSGC <48387484+HenriqueSGC@users.noreply.github.com>
2019-05-10 11:40:21 -0300

    Aula 09

--- Aula-06/Assets/GameFiles/Scripts/BluetoothController.cs
+++ Aula-06/Assets/GameFiles/Scripts/BluetoothController.cs
@@ -1,8 +1,9 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TechTweaking.Bluetooth;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class BluetoothController : MonoBehaviour
 {
@@ -16,6 +17,9 @@ public class BluetoothController : MonoBehaviour
 
 		device = new BluetoothDevice();
 		device.Name = "BT_Controller01";
+		device.setEndByte(255);
+		device.ReadingCoroutine = ManageConnection;
+		DontDestroyOnLoad(this.gameObject);
 	}
 
 	public void connect()
@@ -28,6 +32,12 @@ public class BluetoothController : MonoBehaviour
 		} else{
 			statusText.text = "Connection failed";
 		}
+	}
+
+	public void Play()
+	{
+		Debug.Log("oi");
+		SceneManager.LoadScene("CenaPrincipal");
 	}
 
 	public void disconnect()
@@ -47,6 +57,33 @@ public class BluetoothController : MonoBehaviour
         
     }
 
+    IEnumerator ManageConnection(BluetoothDevice device)
+    {
+    	statusText.text = "Status: Connected & Can Read";
+    	while(device.IsConnected && device.IsReading)
+    	{
+    		BtPackets packets = device.readAllPackets();
+
+    		if(packets != null)
+    		{
+    			int n = packets.Count - 1;
+    			int index = packets.get_packet_offset_index(n);
+    			int size = packets.get_packet_size(n);
+
+    			if(size == 2)
+    			{
+    				int fire = (packets.Buffer[index + 1] << 8) | packets.Buffer[index];
+    				fire = fire >> 3;
+
+    				statusText.text = fire.ToString();
+
+    			}
+    		}
+
+    		yield return null;
+    	}
+    }
+
     // Update is called once per frame
     void Update()
     {
@@ -58,4 +95,6 @@ public class BluetoothController : MonoBehaviour
         	}
         }
     }
+
+
 }
