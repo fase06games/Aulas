4ae1a0fbeb84b38dfb1660a8791dc83fbf78ee50
HenriqueSGC <48387484+HenriqueSGC@users.noreply.github.com>
2019-05-13 14:28:04 -0300

    Aula 02

---
+++ Aula-01/Assets/GameFiles/Scripts/PlayerAction.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAction : MonoBehaviour
+{
+
+	public GameObject bullet;
+	public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+        	GameObject createdBullet = Instantiate(bullet);
+        	createdBullet.transform.position = transform.position;
+
+        	Rigidbody rbullet = createdBullet.GetComponent<Rigidbody>();
+        	Camera cam = GetComponentInChildren<Camera>();
+        	rbullet.velocity = cam.transform.rotation * Vector3.forward * speed;
+        }
+    }
+}
