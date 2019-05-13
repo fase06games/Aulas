ef8df236a4c4608e26d29370be5cbb129ab77d00
HenriqueSGC <48387484+HenriqueSGC@users.noreply.github.com>
2019-05-13 17:40:52 -0300

    Aula 03

--- Aula-01/Assets/GameFiles/Scripts/PlayerAction.cs
+++ Aula-01/Assets/GameFiles/Scripts/PlayerAction.cs
@@ -8,6 +8,8 @@ public class PlayerAction : MonoBehaviour
 	public GameObject bullet;
 	public float speed;
 
+	public ArduinoListener arduino = null;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,14 +19,23 @@ public class PlayerAction : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButtonDown("Fire1"))
+    	if(Input.GetButtonDown("Fire1") || arduino.isShooting() == true)
         {
+        	Shoot();
+        }
+    }
+
+
+    void Shoot()
+    {
+
         	GameObject createdBullet = Instantiate(bullet);
         	createdBullet.transform.position = transform.position;
 
         	Rigidbody rbullet = createdBullet.GetComponent<Rigidbody>();
         	Camera cam = GetComponentInChildren<Camera>();
         	rbullet.velocity = cam.transform.rotation * Vector3.forward * speed;
-        }
-    }
+        }    	
+
+    
 }
