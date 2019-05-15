62f50147aaec4bcac9c1180c418e0544a297cdb4
HenriqueSGC <48387484+HenriqueSGC@users.noreply.github.com>
2019-05-15 15:34:55 -0300

    Aula 04

---
+++ Aula-01/Assets/GameFiles/Scripts/MonsterCatch.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterCatch : MonoBehaviour
+{
+    void OnCollisionEnter(Collision col)
+    {
+    	if(col.gameObject.tag == "Monster")
+    	{
+    		Destroy(col.gameObject);
+    		Destroy(gameObject);
+    	}
+    }
+}
