a47b10f32cd42e7ff5316cc73693fa326bd0bfd3
HenriqueSGC <48387484+HenriqueSGC@users.noreply.github.com>
2019-05-17 16:53:32 -0300

    Aula 08

--- Aula-08/Assets/GameFiles/Scripts/MonsterCatch.cs
+++ Aula-08/Assets/GameFiles/Scripts/MonsterCatch.cs
@@ -4,12 +4,21 @@ using UnityEngine;
 
 public class MonsterCatch : MonoBehaviour
 {
+
+	MonsterCounter monsterCounter;
+
+	void Start()
+	{
+		monsterCounter = FindObjectOfType<MonsterCounter>();
+	}
+
     void OnCollisionEnter(Collision col)
     {
     	if(col.gameObject.tag == "Monster")
     	{
     		Destroy(col.gameObject);
     		Destroy(gameObject);
+    		monsterCounter.increment();
     	}
     }
 }
